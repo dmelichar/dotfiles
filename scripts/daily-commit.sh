ac42896a36b7a6625f103790d1e746d5e06bdf3a
Daniel Melichar <daniel@melichar.xyz>
2021-02-20 08:43:39 +0100

    Added daily commit

---
+++ scripts/daily-commit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /Users/dmelichar/kb/journal/
+git add 2021
+git commit -m "Daily commit"
+git push
+cd /Users/dmelichar/kb
+git add proj prod-sys manifesto leisure journal essays
+git commit -m "Daily commit"
+git push
+
